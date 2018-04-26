4e777ef00e067192978a2fdeab89062bd1db2e7d
Stefan Willems <sgpwille@avans.nl>
2018-04-26 12:14:33 +0200

    poule and tournament implement championship

--- src/app/models/poule.ts
+++ src/app/models/poule.ts
@@ -1,2 +1,11 @@
-export class Poule {
+import {IChampionship} from '../interfaces/ichampionship';
+import {Heat} from './heat';
+import {Participant} from './participant';
+
+export class Poule implements IChampionship {
+  generateHeats: () => void;
+  heats: Heat[];
+  numberOfHeats: number;
+  participants: Participant[];
+  winner: Participant;
 }
--- src/app/models/tournament.ts
+++ src/app/models/tournament.ts
@@ -1,2 +1,11 @@
-export class Tournament {
+import {IChampionship} from '../interfaces/ichampionship';
+import {Heat} from './heat';
+import {Participant} from './participant';
+
+export class Tournament implements IChampionship {
+  generateHeats: () => void;
+  heats: Heat[];
+  numberOfHeats: number;
+  participants: Participant[];
+  winner: Participant;
 }
