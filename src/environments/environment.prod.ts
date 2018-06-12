9dec2564bf1a998ff4ca5eea75b4b807e8808145
unknown <Jahollan@avans.nl>
2018-06-12 13:22:46 +0200

    new start

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBUnxClf5GoSlQ2i62jo3K828nQtLsbvd0',
+    authDomain: 'webs6-d5948.firebaseapp.com',
+    databaseURL: 'https://webs6-d5948.firebaseio.com',
+    projectId: 'webs6-d5948',
+    storageBucket: '',
+    messagingSenderId: '628925580795'
+  }
 };
